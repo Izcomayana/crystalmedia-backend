7201fbbe75ce1fc8c1c2d5ab45c45a265e73bee8
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-10-10 21:23:49 +0100

    fix

--- src/api/lorem/controllers/lorem.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * lorem controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::lorem.lorem');
--- src/api/lorem/routes/lorem.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * lorem router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::lorem.lorem');
