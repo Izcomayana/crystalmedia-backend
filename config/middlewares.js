cf5afc18d66d197f05264fac0d141482def78c53
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-10-11 10:32:44 +0100

    fix middleware to accept imgs from clouinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,38 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
   'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com'],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
+
+
+
+// module.exports = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
